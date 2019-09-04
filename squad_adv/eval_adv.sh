4a3338d200c489c9a22f2f70a2d67cc75ba984cf
exe1023 <exe102343@gmail.com>
2019-09-04 21:23:29 +0800

    experiment code

---
+++ squad_adv/eval_adv.sh
@@ -0,0 +1,5 @@
+#OUTPUT_DIR=../squad_output
+OUTPUT_DIR=../squad_info_output
+python2 eval_squad.py sample1k-HCVerifyAll.json $OUTPUT_DIR/predictions.json
+python2 eval_squad.py sample1k-HCVerifySample.json $OUTPUT_DIR/predictions.json
+
---
+++ train_squad.sh
@@ -0,0 +1,19 @@
+export SQUAD_DIR=squad
+export SQUAD_ADV=squad_adv
+
+python3 run_squad.py \
+  --bert_model bert-base-uncased \
+  --do_train \
+  --do_predict \
+  --do_lower_case \
+  --train_file $SQUAD_DIR/train-v1.1.json \
+  --predict_file $SQUAD_ADV/sample1k-HCVerifyAll.json \
+  --train_batch_size 5 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 2.0 \
+  --max_seq_length 384 \
+  --doc_stride 128 \
+  --output_dir squad_info_output \
+  --seed 10040
+  #--freeze_train
+  #--fp16
